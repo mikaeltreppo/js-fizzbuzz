bafa2beff3d0fd0c443a02b9cbc64e0a0868aab3
mikaeltreppo <mikaeltreppo@outlook.com>
2023-02-24 15:57:49 +0100

    aggiunta della griglia completata

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
+  const containerDom = document.querySelector('.container');
 
-
-for (let i = 1; i <= 100; i++) {
+/*for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
         console.log("fizzbuzz");
     }
@@ -14,4 +14,26 @@ for (let i = 1; i <= 100; i++) {
     else {
         console.log(i);
     }
-};
+};*/
+
+for(let i = 1; i <= 100; i++){
+    const box = document.createElement('div');
+    box.classList.add('square');
+    box.append(i);
+    containerDom.append(box);
+    if (i % 3 == 0 && i % 5 == 0) {
+       box.innerHTML = ("fizzbuzz");
+    }
+    else if (i % 5 == 0) {
+        box.innerHTML = ("buzz");
+    }
+    else if (i % 3 == 0) {
+      box.innerHTML = ("fizz")
+    }
+    else {
+        box.innerHTML = (i);
+    }
+
+}
+  
+   
