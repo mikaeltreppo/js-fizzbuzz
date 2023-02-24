a806fdeed48f095f0018d7baf5b89add8a88cfc4
mikaeltreppo <mikaeltreppo@outlook.com>
2023-02-24 15:21:19 +0100

    Test

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+
+
+for (let i = 1; i <= 100; i++) {
+    console.log(i)
+    if (i % 3 == 0) {
+        console.log(fizz)
+    }
+
+    else if (i % 5 == 0) {
+        console.log("buzz")
+    }
+    else if (i % 3 == 0 && i % 5 == 0) {
+        console.log("fizzbuzz")
+    }
+    else{
+        console.log(i)
+    }
+};
